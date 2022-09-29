cfd042f1971c9ffd118e1ad03fa7985d8b44a56c
Brian Wilcox <brianwilcox@gmail.com>
2022-09-28 18:24:37 -0700

    Fixes

--- cloud/crack.sh
+++ cloud/crack.sh
@@ -2,8 +2,8 @@
 echo "############################################################"
 echo "##             Trying the Phone Number Attack             ##"
 echo "############################################################"
-hashcat -m 22000 $0 -w 3 -a3 -1 "?d" "415?1?1?1?1?1?1?1"
-hashcat -m 22000 $0 -w 3 -a3 -1 "?d" "628?1?1?1?1?1?1?1"
+hashcat -m 22000 $1 -w 3 -a3 -1 "?d" "415?1?1?1?1?1?1?1"
+hashcat -m 22000 $1 -w 3 -a3 -1 "?d" "628?1?1?1?1?1?1?1"
 
 
 echo "############################################################"
@@ -12,7 +12,7 @@ echo "############################################################"
 wget https://github.com/brannondorsey/naive-hashcat/releases/download/data/rockyou.txt
 ## wget https://raw.githubusercontent.com/hashcat/hashcat/master/rules/dive.rule
 wget https://raw.githubusercontent.com/NotSoSecure/password_cracking_rules/master/OneRuleToRuleThemAll.rule
-hashcat -a 0 -m 22000 $0 -w 3 -r OneRuleToRuleThemAll.rule rockyou.txt
+hashcat -a 0 -m 22000 $1 -w 3 -r OneRuleToRuleThemAll.rule rockyou.txt
 
 
 echo "############################################################"
@@ -21,4 +21,4 @@ echo "############################################################"
 wget https://f002.backblazeb2.com/file/passphrase-wordlist/passphrases.txt
 wget https://github.com/initstring/passphrase-wordlist/blob/master/hashcat-rules/passphrase-rule1.rule
 wget https://github.com/initstring/passphrase-wordlist/blob/master/hashcat-rules/passphrase-rule2.rule
-hashcat -a 0 -m 22000 $0 passphrases.txt -r passphrase-rule1.rule -r passphrase-rule2.rule -w 3
+hashcat -a 0 -m 22000 $1 passphrases.txt -r passphrase-rule1.rule -r passphrase-rule2.rule -w 3
