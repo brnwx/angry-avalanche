283657e0f6e93cbf5ee3b706601650defaad552a
Brian Wilcox <brianwilcox@gmail.com>
2022-09-28 19:07:46 -0700

    rule was too long

--- cloud/crack.sh
+++ cloud/crack.sh
@@ -10,8 +10,8 @@ echo "############################################################"
 echo "##              Trying the Rockyou Wordlist               ##"
 echo "############################################################"
 wget https://github.com/brannondorsey/naive-hashcat/releases/download/data/rockyou.txt
-## wget https://raw.githubusercontent.com/hashcat/hashcat/master/rules/dive.rule
-wget https://raw.githubusercontent.com/NotSoSecure/password_cracking_rules/master/OneRuleToRuleThemAll.rule
+wget https://raw.githubusercontent.com/hashcat/hashcat/master/rules/dive.rule
+# wget https://raw.githubusercontent.com/NotSoSecure/password_cracking_rules/master/OneRuleToRuleThemAll.rule
 hashcat -a 0 -m 22000 $1 -w 3 -r OneRuleToRuleThemAll.rule rockyou.txt
 
 
