789782f591b817943544a0029b01394c73488657
Brian Wilcox <brianwilcox@gmail.com>
2023-05-02 18:26:40 -0700

    Fixed some file paths

--- local/quick_crack.sh
+++ local/quick_crack.sh
@@ -8,7 +8,7 @@ echo "💀 |   Trying all 415 numbers"
 hashcat -m 22000 $1 -w 3  --quiet --quiet -a3 -1 "?d" "415?1?1?1?1?1?1?1"
 echo "💀 |   Trying all 628 numbers"
 hashcat -m 22000 $1 -w 3  --quiet --quiet -a3 -1 "?d" "628?1?1?1?1?1?1?1"
-if [ -f "$HOME/sec/src/SecLists/LICENSE" ]; then
+if [ -f "$HOME/src/SecLists/LICENSE" ]; then
     echo "danielmiessler/SecLists files found."
 else
     exit
@@ -18,43 +18,43 @@ echo "############################################################"
 echo "##             Trying WPA Top 4800 Passwords              ##"
 echo "############################################################"
 echo
-hashcat -m 22000 $1 -w 3  --quiet -r $HOME/sec/rules/best64.rule $HOME/sec/src/SecLists/Passwords/WiFi-WPA/probable-v2-wpa-top4800.txt
+hashcat -m 22000 $1 -w 3  --quiet -r $HOME/sec/rules/best64.rule $HOME/src/SecLists/Passwords/WiFi-WPA/probable-v2-wpa-top4800.txt
 echo
 echo "############################################################"
 echo "##           Trying 10K Most Common Passwords             ##"
 echo "############################################################"
 echo
-hashcat -m 22000 $1 -w 3  --quiet -r $HOME/sec/rules/best64.rule $HOME/sec/src/SecLists/Passwords/Common-Credentials/10k-most-common.txt
+hashcat -m 22000 $1 -w 3  --quiet -r $HOME/sec/rules/best64.rule $HOME/src/SecLists/Passwords/Common-Credentials/10k-most-common.txt
 echo
 echo "############################################################"
 echo "##        Trying 100K Most Common NCSC Passwords          ##" 
 echo "############################################################"
 echo
-hashcat -m 22000 $1 -w 3  --quiet -r $HOME/sec/rules/best64.rule $HOME/sec/src/SecLists/Passwords/Common-Credentials/100k-most-used-passwords-NCSC.txt
+hashcat -m 22000 $1 -w 3  --quiet -r $HOME/sec/rules/best64.rule $HOME/src/SecLists/Passwords/Common-Credentials/100k-most-used-passwords-NCSC.txt
 echo
 echo "############################################################"
 echo "##              Trying 2017 Darweb Top 10K                ##"
 echo "############################################################"
 echo
-hashcat -m 22000 $1 -w 3  --quiet -r $HOME/sec/rules/best64.rule $HOME/sec/src/SecLists/Passwords/darkweb2017-top10000.txt
+hashcat -m 22000 $1 -w 3  --quiet -r $HOME/sec/rules/best64.rule $HOME/src/SecLists/Passwords/darkweb2017-top10000.txt
 echo
 echo "############################################################"
 echo "##              Trying the Rockyou Wordlist               ##"
 echo "############################################################"
 echo
-hashcat -m 22000 $1 -w 3  --quiet -r $HOME/sec/rules/best64.rule rockyou.txt
+hashcat -m 22000 $1 -w 3  --quiet -r $HOME/sec/rules/best64.rule $HOME/sec/rockyou.txt
 echo
 echo "############################################################"
 echo "##     Trying the Common Credentials Top 10 million       ##"
 echo "############################################################"
 echo
-hashcat -m 22000 $1 -w 3  --quiet -r $HOME/sec/rules/best64.rule $HOME/sec/src/SecLists/Passwords/Common-Credentials/10-million-password-list-top-1000000.txt
+hashcat -m 22000 $1 -w 3  --quiet -r $HOME/sec/rules/best64.rule $HOME/src/SecLists/Passwords/Common-Credentials/10-million-password-list-top-1000000.txt
 echo
 echo "############################################################"
 echo "##          Trying the Xatonet Top 10 million             ##"
 echo "############################################################"
 echo
-hashcat -m 22000 $1 -w 3  --quiet -r $HOME/sec/rules/best64.rule $HOME/sec/src/SecLists/Passwords/xato-net-10-million-passwords.txt
+hashcat -m 22000 $1 -w 3  --quiet -r $HOME/sec/rules/best64.rule $HOME/src/SecLists/Passwords/xato-net-10-million-passwords.txt
 echo
 echo "############################################################"
 echo "##             Executing Passphrase Attack                ##"
