1b1b5e9612bb08976ca53c109179501756a511ab
Brian Wilcox <brianwilcox@gmail.com>
2023-11-11 15:44:33 -0800

    lint error

--- kali/convert.sh
+++ kali/convert.sh
@@ -2,4 +2,4 @@
 
 read -p "output prefix: " file_prefix
 
-hcxpcapngtool -o $file_prefix.hashcat.22000 -E essidlist $file_prefix.pcapng
+hcxpcapngtool -o $file_prefix.22000 -E essidlist $file_prefix.pcapng
