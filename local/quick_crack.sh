ae5bc4efabe47452a295d042352e54d8e56fe491
Brian Wilcox <brianwilcox@gmail.com>
2023-11-11 12:03:41 -0800

    Update to rule best66.rule

--- local/quick_crack.sh
+++ local/quick_crack.sh
@@ -18,43 +18,43 @@ echo "############################################################"
 echo "##             Trying WPA Top 4800 Passwords              ##"
 echo "############################################################"
 echo
-hashcat -m 22000 $1 -w 3  --quiet -r $HOME/sec/rules/best64.rule $HOME/src/SecLists/Passwords/WiFi-WPA/probable-v2-wpa-top4800.txt
+hashcat -m 22000 $1 -w 3  --quiet -r $HOME/sec/rules/best66.rule $HOME/src/SecLists/Passwords/WiFi-WPA/probable-v2-wpa-top4800.txt
 echo
 echo "############################################################"
 echo "##           Trying 10K Most Common Passwords             ##"
 echo "############################################################"
 echo
-hashcat -m 22000 $1 -w 3  --quiet -r $HOME/sec/rules/best64.rule $HOME/src/SecLists/Passwords/Common-Credentials/10k-most-common.txt
+hashcat -m 22000 $1 -w 3  --quiet -r $HOME/sec/rules/best66.rule $HOME/src/SecLists/Passwords/Common-Credentials/10k-most-common.txt
 echo
 echo "############################################################"
 echo "##        Trying 100K Most Common NCSC Passwords          ##" 
 echo "############################################################"
 echo
-hashcat -m 22000 $1 -w 3  --quiet -r $HOME/sec/rules/best64.rule $HOME/src/SecLists/Passwords/Common-Credentials/100k-most-used-passwords-NCSC.txt
+hashcat -m 22000 $1 -w 3  --quiet -r $HOME/sec/rules/best66.rule $HOME/src/SecLists/Passwords/Common-Credentials/100k-most-used-passwords-NCSC.txt
 echo
 echo "############################################################"
 echo "##              Trying 2017 Darweb Top 10K                ##"
 echo "############################################################"
 echo
-hashcat -m 22000 $1 -w 3  --quiet -r $HOME/sec/rules/best64.rule $HOME/src/SecLists/Passwords/darkweb2017-top10000.txt
+hashcat -m 22000 $1 -w 3  --quiet -r $HOME/sec/rules/best66.rule $HOME/src/SecLists/Passwords/darkweb2017-top10000.txt
 echo
 echo "############################################################"
 echo "##              Trying the Rockyou Wordlist               ##"
 echo "############################################################"
 echo
-hashcat -m 22000 $1 -w 3  --quiet -r $HOME/sec/rules/best64.rule $HOME/sec/rockyou.txt
+hashcat -m 22000 $1 -w 3  --quiet -r $HOME/sec/rules/best66.rule $HOME/sec/rockyou.txt
 echo
 echo "############################################################"
 echo "##     Trying the Common Credentials Top 10 million       ##"
 echo "############################################################"
 echo
-hashcat -m 22000 $1 -w 3  --quiet -r $HOME/sec/rules/best64.rule $HOME/src/SecLists/Passwords/Common-Credentials/10-million-password-list-top-1000000.txt
+hashcat -m 22000 $1 -w 3  --quiet -r $HOME/sec/rules/best66.rule $HOME/src/SecLists/Passwords/Common-Credentials/10-million-password-list-top-1000000.txt
 echo
 echo "############################################################"
 echo "##          Trying the Xatonet Top 10 million             ##"
 echo "############################################################"
 echo
-hashcat -m 22000 $1 -w 3  --quiet -r $HOME/sec/rules/best64.rule $HOME/src/SecLists/Passwords/xato-net-10-million-passwords.txt
+hashcat -m 22000 $1 -w 3  --quiet -r $HOME/sec/rules/best66.rule $HOME/src/SecLists/Passwords/xato-net-10-million-passwords.txt
 echo
 echo "############################################################"
 echo "##             Executing Passphrase Attack                ##"
